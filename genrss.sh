8bd884bc70bdc8eda1304348d06ba5eb049345bc
TaiKedz <dch.tai@gmail.com>
2014-12-11 21:52:05 +0000

    Cleaned up some sections, added license text, made sure the identifier in the prefs holds on one line, ensure that the name of the feed is URL-friendly...

--- genrss.sh
+++ genrss.sh
@@ -1,5 +1,26 @@
 #! /bin/bash
 
+# Copyright 2014 Tai Kedzierski
+# https://github.com/taikedz/watcher-rss
+#
+# This code is licensed under the GNU Affero General Public License v3.0
+# https://www.gnu.org/licenses/agpl-3.0.html
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# ====================================================
+
 # To use this script
 # `crontab -e`
 # then add an entry at the desired frequency
@@ -11,11 +32,6 @@
 GENRSSRC="$HOME/.genrss"
 DBFILE=rss-db
 DEFRSSDIR="$GENRSSRC/feeds"
-
-function dolog() {
-        # echo $@
-        :
-}
 
 function trymkdir() {
         mkdir -p $1
@@ -31,8 +47,6 @@ if [[ ! -d $GENRSSRC ]]; then
         touch "$GENRSSRC/$DBFILE"
 fi
 
-dolog Passed Dir creationg
-
 # these functions can be overidden in definition file
 function getpermalink() {
 echo "$PAGEURL"
@@ -42,42 +56,39 @@ echo "<a href=\"$PAGEURL\">$PAGENAME</a>"
 }
 
 function getpage() {
-# getpage pageurl localfile
-        dolog URL -- $1
-        dolog TMP -- $2
+	# getpage pageurl localfile
         wget -q "$1" --header='User-Agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0' --output-document="$2"
 }
 
 PAGEID=$1
 PAGEPREFS="$GENRSSRC/$PAGEID"
 
-# this is where we store the configs for the page
+# this is where we load the configs for the specified page
 # Defining:
 #   findstamp() - identify in /tmp/genrss-tempfile the string
-#        identifying new content, and return that identifying string
+#        identifying new content, and return md5 of that identifying string
 #   $PAGENAME - the name of the rss feed file
 #   $PAGEURL - the URL of the page
 #
 #   getpermalink() - permalink - optional
 #   getdescription() - extract a portion of the page - optional
 source "$PAGEPREFS"
-dolog source loaded $PAGEID - $PAGEURL - $PAGENAME - $(findstamp)
 
-dolog getting page...
-getpage "$PAGEURL" /tmp/genrss-tempfile
-dolog Got page.
+TMPFILE=$(mktemp)
+getpage "$PAGEURL" $TMPFILE
 
 CURDATE=$(date)
 LASTSTAMP=$(cat "$GENRSSRC/$DBFILE" | grep "$PAGEID:" | sed -r -e "s/$PAGEID:(.+)\$/\1/")
 
-NEWSTAMP=$(findstamp)
+NEWSTAMP=$(findstamp | head -n 1) # ensure it sits only on one line
 GENLINK=$(getpermalink)
 PAGEDESCRIPTION=$(getdescription)
 
 if [[ $LASTSTAMP != $NEWSTAMP ]]; then
         # save the new identifier
         sed -i -r -e "s/$PAGEID:.+$/$PAGEID:$NEWSTAMP/" "$GENRSSRC/$DBFILE"
-        cat <<EOFEED > "$DEFRSSDIR/$PAGENAME.xml"
+	RSSNAME=$(echo $PAGENAME | sed -r -e s/[^a-zA-Z0-9]+/_/)
+        cat <<EOFEED > "$DEFRSSDIR/$RSSNAME.xml"
 <?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
@@ -95,14 +106,12 @@ if [[ $LASTSTAMP != $NEWSTAMP ]]; then
 </channel>
 </rss>
 EOFEED
-else
-        dolog No change.
 fi
 
-# first time we've parsed this comic
+# first time we've parsed this page/profile
 if [[ "x$LASTSTAMP" = "x" ]]; then
         echo "$PAGEID:$NEWSTAMP" > "$GENRSSRC/$DBFILE"
 fi
 
 # cleanup temp
-rm /tmp/genrss-tempfile
+rm $TMPFILE
